4d9eaa3003328e7892f1cca841419f120c06c560
Andrew Sweet <andrewmsweet@gmail.com>
2016-05-28 12:05:47 -0700

    Setting up map drawing

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,5 +2,35 @@ requirejs([
 ], 
 function(
 ) {
-  console.log('test!');
+  var map;
+
+  window.onGoogleMapsLoad = _init;
+
+  function _init() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: 40.4585954,
+        lng: -79.9691057
+      },
+      zoom: 7
+    });
+
+    var drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.POLYGON,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [
+          google.maps.drawing.OverlayType.CIRCLE,
+          google.maps.drawing.OverlayType.POLYGON,
+          google.maps.drawing.OverlayType.RECTANGLE
+        ]
+      }
+    });
+    drawingManager.setMap(map);
+  }
+
+  return {
+    init : _init
+  }
 });
